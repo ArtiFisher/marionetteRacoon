e27f784e47a07916da88fad8f90abbadff9192a0
Fisher <artemis.fisher@gmail.com>
2015-03-13 11:23:07 +0300

    seems ok

--- scripts/mainMarionette.js
+++ scripts/mainMarionette.js
@@ -48,17 +48,46 @@
 
     Racoon.Views.One = Backbone.Marionette.ItemView.extend({
         template:'#onePost',
-        className: 'lonely'
+        className: 'lonely',
+        events: {
+            'click .edit': 'edit',
+            'click .save': 'save'
+        },
+        ui: {
+            title: '#oneTitle',
+            text: '#oneText',
+            url: '#oneUrl',
+            date: '#oneDate',
+            image: '#oneImage'
+        },
+        edit: function() {
+            $('.lonely').addClass('editing');
+            $('.lonely input,textarea').prop('disabled', false);
+        },
+        save: function() {
+            $('.lonely').removeClass('editing');
+            $('.lonely input,textarea').prop('disabled', true);
+            this.model.attributes.title = this.ui.title.val();
+            this.model.attributes.text = this.ui.text.val();
+            this.model.attributes.image = this.ui.url.val();
+            this.model.attributes.date = new Date();
+            this.model.url = Racoon.App.posts.url + '/' + this.model.id;
+            this.ui.date.html(Racoon.dateFilter(this.model.attributes.date));
+            this.ui.image.attr('src', this.model.attributes.image);
+            this.model.save();
+        }
     });
 
     Racoon.Views.Post = Backbone.Marionette.ItemView.extend({
         template: template('blogPost'),
         tagName: 'article',
         events: {
-            'click .destroy': 'destroy'
+            'click .remove': 'removePost'
         },
-        destroy: function() {
-            
+        removePost: function() {
+            this.model.url = this.model.collection.url + '/' + this.model.id;
+            this.model.collection.remove(this.model);
+            this.model.destroy();
         }
     });
 
@@ -125,7 +154,7 @@
             if(!Racoon.changed){
                 Racoon.App.list.show(new Racoon.Views.Posts({collection: Racoon.App.posts}));
                 $('#one').hide();
-                $('aside,.content').show();
+                $('aside,.content,.add').show();
             }
             else{
                 Racoon.changed = false;
@@ -134,7 +163,7 @@
                         Racoon.App.list.show(new Racoon.Views.Posts({collection: Racoon.App.posts}));
                         Racoon.changed = false;
                         $('#one').hide();
-                        $('aside,.content').show();
+                        $('aside,.content,.add').show();
                     }
                 });
             }      
